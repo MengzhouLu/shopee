4d31526d66e4a055e4cd5dedcd4fb4837c69e43d
“Mengzhou” <Mengzhou1314@163.com>
2024-05-13 15:56:16 +0800

    testV2.0 更换bert模型

--- testV2.0.py
+++ testV2.0.py
@@ -387,70 +387,70 @@ text_embeddings = cp.array(text_embeddings)  # 使用了 CuPy 库来进行大规
 
 tmp = test.groupby('label_group').posting_id.agg('unique').to_dict()
 test['target'] = test.label_group.map(tmp)
-for threshold in [0.57,0.58,0.59,0.6,0.61,0.62,0.63,0.64]:
-    print(f"threshold: {threshold}")
-    preds = []
-    CHUNK = 1024 * 4*4
+threshold=0.58
+print(f"threshold: {threshold}")
+preds = []
+CHUNK = 1024 * 4*4
 
-    print('Finding similar titles...')
-    CTS = len(test) // CHUNK
-    if len(test) % CHUNK != 0: CTS += 1
-    for j in range(CTS):
-        a = j * CHUNK
-        b = min((j + 1) * CHUNK, len(test))
-        print('chunk', a, 'to', b)
-        # 寻找相似的邻居
-        distances, indices = model.kneighbors(text_embeddings[a:b], n_neighbors=KNN)
-        # 将距离转换为相似度
-        similarities = 1 / (1 + distances)
+print('Finding similar titles...')
+CTS = len(test) // CHUNK
+if len(test) % CHUNK != 0: CTS += 1
+for j in range(CTS):
+    a = j * CHUNK
+    b = min((j + 1) * CHUNK, len(test))
+    print('chunk', a, 'to', b)
+    # 寻找相似的邻居
+    distances, indices = model.kneighbors(text_embeddings[a:b], n_neighbors=KNN)
+    # 将距离转换为相似度
+    similarities = 1 / (1 + distances)
 
-        for k in range(b - a):
-            IDX = cp.where(similarities[k,] > threshold)[0]
-            o = test.iloc[cp.asnumpy(indices[k, IDX])].posting_id.values
-            preds.append(o)
+    for k in range(b - a):
+        IDX = cp.where(similarities[k,] > threshold)[0]
+        o = test.iloc[cp.asnumpy(indices[k, IDX])].posting_id.values
+        preds.append(o)
 
-        del distances, indices
-    # text_embeddings = text_embeddings.get()
-    # del text_embeddings
-    # cp.get_default_memory_pool().free_all_blocks()  # 释放显存
-    # _ = gc.collect()
+    del distances, indices
+text_embeddings = text_embeddings.get()
+del text_embeddings
+cp.get_default_memory_pool().free_all_blocks()  # 释放显存
+_ = gc.collect()
 
-    test['preds'] = preds
-    test.head()
-    print("CV for text  :", round(test.apply(getMetric('preds'), axis=1).mean(), 3))
-
+test['preds'] = preds
+test.head()
 
 
-# tmp = test.groupby('image_phash').posting_id.agg('unique').to_dict()
-# test['preds3'] = test.image_phash.map(tmp)
-# test.head()
 
+
+tmp = test.groupby('image_phash').posting_id.agg('unique').to_dict()
+test['preds3'] = test.image_phash.map(tmp)
+test.head()
+
+
+def combine_for_sub(row):
+    x = np.concatenate([row.preds, row.preds2])
+    return ' '.join(np.unique(x))
+
+
+def combine_for_cv(row):
+    x = np.concatenate([row.preds, row.preds2])
+    return np.unique(x)
+
+
+# if COMPUTE_CV:
+#     tmp = test.groupby('label_group').posting_id.agg('unique').to_dict()
+#     test['target'] = test.label_group.map(tmp)
+#     test['oof'] = test.apply(combine_for_cv, axis=1)
+#     test['f1'] = test.apply(getMetric('oof'), axis=1)
+#     print('CV Score =', test.f1.mean())
 #
-# def combine_for_sub(row):
-#     x = np.concatenate([row.preds, row.preds2])
-#     return ' '.join(np.unique(x))
-#
-#
-# def combine_for_cv(row):
-#     x = np.concatenate([row.preds, row.preds2])
-#     return np.unique(x)
-#
-#
-# # if COMPUTE_CV:
-# #     tmp = test.groupby('label_group').posting_id.agg('unique').to_dict()
-# #     test['target'] = test.label_group.map(tmp)
-# #     test['oof'] = test.apply(combine_for_cv, axis=1)
-# #     test['f1'] = test.apply(getMetric('oof'), axis=1)
-# #     print('CV Score =', test.f1.mean())
-# #
-# # test['matches'] = test.apply(combine_for_sub, axis=1)
-#
-# # print("CV for image :", round(test.apply(getMetric('preds2'), axis=1).mean(), 3))
-# print("CV for text  :", round(test.apply(getMetric('preds'), axis=1).mean(), 3))
-# print("CV for phash :", round(test.apply(getMetric('preds3'), axis=1).mean(), 3))
-#
-# test
-#
-# test[['posting_id', 'matches']].to_csv('submission.csv', index=False)
-# sub = pd.read_csv('submission.csv')
-# sub.head()
+# test['matches'] = test.apply(combine_for_sub, axis=1)
+
+# print("CV for image :", round(test.apply(getMetric('preds2'), axis=1).mean(), 3))
+print("CV for text  :", round(test.apply(getMetric('preds'), axis=1).mean(), 3))
+print("CV for phash :", round(test.apply(getMetric('preds3'), axis=1).mean(), 3))
+
+test
+
+test[['posting_id', 'matches']].to_csv('submission.csv', index=False)
+sub = pd.read_csv('submission.csv')
+sub.head()
