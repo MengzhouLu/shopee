8a3fd678d3f9cf23f0eaf9b67f36c77db5eccd6d
“Mengzhou” <Mengzhou1314@163.com>
2024-05-13 17:23:58 +0800

    testV2.1 title with bert_indo

--- testV2.1.py
+++ testV2.1.py
@@ -199,17 +199,17 @@ def load_model(model, model_file):
 import math
 from tqdm import tqdm
 
-# WGT = './b0ns_256_bert_20ep_fold0_epoch27.pth'
-#
-# model = enet_arcface_FINAL('tf_efficientnet_b0_ns', out_dim=11014).cuda()
-# model = load_model(model, WGT)
+WGT = './b0ns_256_bert_20ep_fold0_epoch27.pth'
+
+model = enet_arcface_FINAL('tf_efficientnet_b0_ns', out_dim=11014).cuda()
+image_model = load_model(model, WGT)
 #
 # embeds = []
 #
 # with torch.no_grad():
 #     for img, input_ids, attention_mask in tqdm(test_loader):
 #         img, input_ids, attention_mask = img.cuda(), input_ids.cuda(), attention_mask.cuda()
-#         feat, _ = model(img, input_ids, attention_mask)
+#         feat, _ = image_model(img, input_ids, attention_mask)
 #         image_embeddings = feat.detach().cpu().numpy()
 #         embeds.append(image_embeddings)
 #
@@ -223,6 +223,8 @@ from tqdm import tqdm
 
 with open('image_embeddings.pkl', 'rb') as f:    # Unpickling
     image_embeddings = pickle.load(f)
+
+img_embs=image_embeddings
 
 KNN = 50
 if len(test)==3: KNN = 2
@@ -338,6 +340,7 @@ model = AutoModel.from_pretrained(model_name)
 state = torch.load('./bertmodel/bert_indo_val0.pth')
 model.load_state_dict(state,strict=False)
 model.cuda()
+bert_model=model
 # 准备输入数据
 
 # embeddings = []
@@ -345,7 +348,7 @@ model.cuda()
 # with torch.no_grad():
 #     for title, _ in tqdm(title_loader):
 #         tokens = tokenizer(title, padding='max_length', truncation=True, max_length=100, return_tensors="pt").to('cuda')
-#         outputs = model(**tokens)
+#         outputs = bert_model(**tokens)
 #         sentence_embeddings = outputs.last_hidden_state[:, 0, :]  # 获取[CLS]标记所对应的输出
 #         # embeddings.append(sentence_embeddings.cpu().numpy())
 #         embeddings.append(sentence_embeddings.detach().cpu())
@@ -365,6 +368,7 @@ _ = gc.collect()
 # text_embeddings = text_embeddings.reshape(test.shape[0], -1)
 print(text_embeddings.shape)
 
+bert_embs=text_embeddings
 # model = TfidfVectorizer(stop_words=None,
 #                         binary=True,
 #                         max_features=25000)
@@ -454,3 +458,248 @@ test
 test[['posting_id', 'matches']].to_csv('submission.csv', index=False)
 sub = pd.read_csv('submission.csv')
 sub.head()
+
+def add_target_groups(data_df, source_column='label_group', target_column='target'):
+    target_groups = data_df.groupby(source_column).indices
+    data_df[target_column]=data_df[source_column].map(target_groups)
+    return data_df
+
+def add_splits(train_df, valid_group=0):
+    grouped = train_df.groupby('label_group').size()
+
+    labels, sizes =grouped.index.to_list(), grouped.to_list()
+
+    skf = StratifiedKFold(5)
+    splits = list(skf.split(labels, sizes))
+
+    group_to_split =  dict()
+    for idx in range(5):
+        labs = np.array(labels)[splits[idx][1]]
+        group_to_split.update(dict(zip(labs, [idx]*len(labs))))
+
+    train_df['split'] = train_df.label_group.replace(group_to_split)
+    train_df['is_valid'] = train_df['split'] == valid_group
+    return train_df
+
+def embs_from_model(model, dl):
+    all_embs = []
+    all_ys=[]
+    for batch in tqdm(dl):
+        if len(batch) ==2:
+            bx,by=batch
+        else:
+            bx,=batch
+            by=torch.zeros(1)
+        with torch.no_grad():
+            embs = model(bx)
+            all_embs.append(embs.half())
+        all_ys.append(by)
+    all_embs = F.normalize(torch.cat(all_embs))
+    return all_embs, torch.cat(all_ys)
+
+def get_targets_shape(train_df):
+    all_targets = add_target_groups(train_df).target.to_list()
+    all_targets_lens = [len(t) for t in all_targets]
+    targets_shape = []
+    for size in range(min(all_targets_lens), max(all_targets_lens)+1):
+        count = all_targets_lens.count(size) / len(all_targets)
+        targets_shape.append((size,count))
+    return targets_shape
+
+def chisel(groups, groups_p, pos, target_count):
+    probs = []
+    groups_lens = [len(g)for g in groups]
+    current_count = groups_lens.count(pos)
+    if current_count >= target_count:
+
+        return
+    to_cut = target_count - current_count
+    for i in range(len(groups)):
+        if len(groups_p[i])>pos:
+            probs.append((i, groups_p[i][pos]))
+    probs.sort(key=lambda x:x[1])
+    for i in range(min(to_cut, len(probs))):
+        group_idx = probs[i][0]
+        groups[group_idx]=groups[group_idx][:pos]
+        groups_p[group_idx]=groups_p[group_idx][:pos]
+
+def sorted_pairs(distances, indices):
+    triplets = []
+    n= len(distances)
+    for x in range(n):
+        used=set()
+        for ind, dist in zip(indices[x].tolist(), distances[x].tolist()):
+            if not ind in used:
+                triplets.append((x, ind, dist))
+                used.add(ind)
+    return sorted(triplets, key=lambda x: -x[2])
+def do_chunk(embs):
+    step = 1000
+    for chunk_start in range(0, embs.shape[0], step):
+        chunk_end = min(chunk_start+step, len(embs))
+        yield embs[chunk_start:chunk_end]
+def get_nearest(embs, emb_chunks, K=None, sorted=True):
+    if K is None:
+        K = min(51, len(embs))
+    distances = []
+    indices = []
+    for chunk in emb_chunks:
+        sim = embs @ chunk.T
+        top_vals, top_inds = sim.topk(K, dim=0, sorted=sorted)
+        distances.append(top_vals.T)
+        indices.append(top_inds.T)
+    return torch.cat(distances), torch.cat(indices)
+
+def combined_distances(embs_list):
+    K = min(len(embs_list[0]), 51)
+    combined_inds =[get_nearest(embs, do_chunk(embs))[1] for embs in embs_list]
+    combined_inds = torch.cat(combined_inds, dim=1)
+    res_inds,res_dists = [],[]
+    for x in range(len(combined_inds)):
+        inds = combined_inds[x].unique()
+        Ds = [embs[None,x] @ embs[inds].T for embs in embs_list]
+        D = Ds[0] + Ds[1] - Ds[0] * Ds[1]
+        top_dists, top_inds = D.topk(K)
+        res_inds.append(inds[top_inds])
+        res_dists.append(top_dists)
+    return torch.cat(res_inds), torch.cat(res_dists)
+
+def blend_embs(embs_list, threshold, m2_threshold, data_df):
+    combined_inds, combined_dists = combined_distances(embs_list)
+    check_measurements(combined_dists, combined_inds, data_df)
+    new_embs_list = L((torch.empty_like(embs) for embs in embs_list))
+    for x in range(len(embs_list[0])):
+        neighs = combined_dists[x] > threshold
+        if neighs.sum() == 1 and combined_dists[x][1]>m2_threshold:
+            neighs[1]=1
+        neigh_inds, neigh_ratios = combined_inds[x, neighs], combined_dists[x,neighs]
+        for embs, new_embs in zip(embs_list, new_embs_list):
+            new_embs[x] = (embs[neigh_inds] * neigh_ratios.view(-1,1)).sum(dim=0)
+    return new_embs_list.map(F.normalize)
+
+# Not used in solution, just for illustration purpose
+def f1(tp, fp, num_tar):
+    return 2 * tp / (tp+fp+num_tar)
+
+def build_from_pairs(pairs, target, display = True):
+    score =0
+    tp = [0]*len(target)
+    fp = [0]*len(target)
+    scores=[]
+    vs=[]
+    group_sizes = [len(x) for x in target]
+    for x, y, v in pairs:
+        group_size = group_sizes[x]
+        score -= f1(tp[x], fp[x], group_size)
+        if y in target[x]: tp[x] +=1
+        else: fp[x] +=1
+        score += f1(tp[x], fp[x], group_size)
+        scores.append(score / len(target))
+        vs.append(v)
+    if display:
+        plt.plot(scores)
+        am =torch.tensor(scores).argmax()
+        print(f'{scores[am]:.3f} at {am/len(target)} pairs or {vs[am]:.3f} threshold')
+    return scores
+
+
+def score_distances(dist, targets, display=False):
+    triplets = dist_to_edges(dist)[:len(dist)*10]
+    return max(build_from_pairs(triplets, targets, display))
+
+def score_group(group, target):
+    tp = len(set(group).intersection(set(target)))
+    return 2 * tp / (len(group)+len(target))
+def score_all_groups(groups, targets):
+    scores = [score_group(groups[i], targets[i]) for i in range(len(groups))]
+    return sum(scores)/len(scores)
+def show_groups(groups, targets):
+    groups_lens = [len(g)for g in groups]
+    targets_lens = [len(g) for g in targets]
+    plt.figure(figsize=(8,8))
+    plt.hist((groups_lens,targets_lens) ,bins=list(range(1,52)), label=['preds', 'targets'])
+    plt.legend()
+    plt.title(f'score: {score_all_groups(groups, targets):.3f}')
+    plt.show()
+
+measurements = {
+    'weight': [('mg',1), ('g', 1000), ('gr', 1000), ('gram', 1000), ('kg', 1000000)],
+    'length': [('mm',1), ('cm', 10), ('m',1000), ('meter', 1000)],
+    'pieces': [ ('pc',1)],
+    'memory': [('gb', 1)],
+    'volume': [('ml', 1), ('l', 1000), ('liter',1000)]
+}
+
+def to_num(x, mult=1):
+    x = x.replace(',','.')
+    return int(float(x)*mult)
+
+def extract_unit(tit, m):
+    pat = f'\W(\d+(?:[\,\.]\d+)?) ?{m}s?\W'
+    matches = regex.findall(pat, tit, overlapped=True)
+    return set(matches)
+
+def extract(tit):
+
+    res =dict()
+    tit = ' '+tit.lower()+' '
+    for cat, units in measurements.items():
+        cat_values=set()
+        for unit_name, mult in units:
+            values = extract_unit(tit, unit_name)
+            values = {to_num(v, mult) for v in values}
+            cat_values = cat_values.union(values)
+        if cat_values:
+            res[cat] = cat_values
+    return res
+
+def add_measurements(data):
+    data['measurement'] = data.title.map(extract)
+    return data
+
+def match_measures(m1, m2):
+    k1,k2 = set(m1.keys()), set(m2.keys())
+    common = k1.intersection(k2)
+    if not common: return True
+    for key in common:
+        s1,s2 = m1[key], m2[key]
+        if s1.intersection(s2):
+            return True
+    return False
+
+def check_measurements(combined_dists, combined_inds, data_df):
+    K = min(8, len(data_df)) * len(data_df)
+    _, inds_k = combined_dists.view(-1).topk(K)
+    removed = 0
+    inds_k = inds_k.tolist()
+    for idx in inds_k:
+        x = idx // combined_inds.shape[1]
+        y_idx = idx % combined_inds.shape[1]
+        y = combined_inds[x,y_idx]
+        if not match_measures(data_df.iloc[x].measurement, data_df.iloc[y.item()].measurement):
+            removed +=1
+            combined_dists[x][y_idx]=0
+    print('removed', removed, 'matches')
+
+img_embs,ys = embs_from_model(image_model, test_loader)
+bert_embs, ys = embs_from_model(bert_model, title_loader.valid)
+
+
+valid_df=test
+# thees are thresholds used for Neighborhood Blending
+RECIPROCAL_THRESHOLD = .97
+MIN_PAIR_THRESHOLD = .6
+
+new_embs = blend_embs([img_embs, bert_embs], RECIPROCAL_THRESHOLD, MIN_PAIR_THRESHOLD, valid_df)
+
+set_size = len(img_embs)
+target_matrix= ys[:,None]==ys[None,:]
+targets = [torch.where(t)[0].tolist() for t in target_matrix]
+
+combined_inds, combined_dists = combined_distances([img_embs, bert_embs])
+pairs = sorted_pairs(combined_dists, combined_inds)
+_=build_from_pairs(pairs[:10*len(combined_inds)], targets)
+
+check_measurements(combined_dists, combined_inds, valid_df)
+pairs = sorted_pairs(combined_dists, combined_inds)
+_=build_from_pairs(pairs[:10*len(combined_inds)], targets)
