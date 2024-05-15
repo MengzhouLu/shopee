bb8070c95f13b1489ee14c8bcc618b370979dbbf
“Mengzhou” <Mengzhou1314@163.com>
2024-05-15 15:03:10 +0800

    ？

--- CLIP.py
+++ CLIP.py
@@ -98,8 +98,8 @@ df_train['filepath'] = df_train['image'].apply(lambda x: os.path.join('./', 'tra
 dataset_train = LandmarkDataset(df_train, 'train', 'train')
 train_loader = DataLoader(dataset_train, batch_size=BATCH_SIZE, num_workers=16, shuffle=True,pin_memory=True)
 
-dataset_test = LandmarkDataset(df_sub, 'train', 'test')
-test_loader = DataLoader(dataset_test, batch_size=BATCH_SIZE, num_workers=16, shuffle=False,pin_memory=True)
+dataset_test = LandmarkDataset(df_train, 'train', 'test')#这里的mode是test，所以会截断到63个字
+test_loader = DataLoader(dataset_test, batch_size=BATCH_SIZE, num_workers=16, shuffle=True,pin_memory=True)
 
 # def convert_models_to_fp32(model):
 #     for p in model.parameters():
