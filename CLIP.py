ae4d6d85e5134f24100177f0e74ebdd5b7b414ad
“Mengzhou” <Mengzhou1314@163.com>
2024-05-16 11:58:29 +0800

    CLIP

--- CLIP.py
+++ CLIP.py
@@ -202,39 +202,40 @@ img_embeddings=[]
 text_embeddings=[]
 combine_embeddings=[]
 #由CLIP模型得到的图片和文本的嵌入向量
-# def embs_from_model(model, test_loader):
-#     model.eval()
-#
-#     for batch in tqdm(test_loader):
-#
-#         data = batch
-#         data_images = data["P"].to(device)
-#         data_texts = data["T"]
-#         texts = [f"a photo of a {title}" for title in data_texts]
-#         texts_tokenized = clip.tokenize(texts).to(device)
-#         with torch.no_grad():
-#             image_features = model.encode_image(data_images).float()
-#             text_features = model.encode_text(texts_tokenized).float()
-#             text_features /= text_features.norm(dim=-1, keepdim=True)
-#             image_features /= image_features.norm(dim=-1, keepdim=True)
-#             img_embeddings.append(image_features.cpu().numpy())
-#             text_embeddings.append(text_features.cpu().numpy())
-#             combined_features = torch.cat((image_features, text_features), dim=1)
-#             combine_embeddings.append(combined_features.cpu().numpy())  # 拼接两个向量，作为一个新的向量
-#             print(image_features.shape,text_features.shape,combined_features.shape)
-#
-# embs_from_model(model, test_loader)
-#
-# img_embeddings=np.concatenate(img_embeddings,axis=0)
-# text_embeddings=np.concatenate(text_embeddings,axis=0)
-# combine_embeddings=np.concatenate(combine_embeddings,axis=0)
-# print(img_embeddings.shape,text_embeddings.shape,combine_embeddings.shape)
+def embs_from_model(model, test_loader):
+    model.eval()
+
+    for batch in tqdm(test_loader):
+
+        data = batch
+        data_images = data["P"].to(device)
+        data_texts = data["T"]
+        # texts = [f"a photo of a {title}" for title in data_texts]
+        texts = data_texts
+        texts_tokenized = clip.tokenize(texts).to(device)
+        with torch.no_grad():
+            image_features = model.encode_image(data_images).float()
+            text_features = model.encode_text(texts_tokenized).float()
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            img_embeddings.append(image_features.cpu().numpy())
+            text_embeddings.append(text_features.cpu().numpy())
+            combined_features = torch.cat((image_features, text_features), dim=1)
+            combine_embeddings.append(combined_features.cpu().numpy())  # 拼接两个向量，作为一个新的向量
+            print(image_features.shape,text_features.shape,combined_features.shape)
+
+embs_from_model(model, test_loader)
+
+img_embeddings=np.concatenate(img_embeddings,axis=0)
+text_embeddings=np.concatenate(text_embeddings,axis=0)
+combine_embeddings=np.concatenate(combine_embeddings,axis=0)
+print(img_embeddings.shape,text_embeddings.shape,combine_embeddings.shape)
 
 import pickle
-# with open('image_embeddings_clip.pkl', 'wb') as f:    #Pickling
-#     pickle.dump(img_embeddings, f)
-# with open('text_embeddings_clip.pkl', 'wb') as f:    #Pickling
-#     pickle.dump(text_embeddings, f)
+with open('image_embeddings_clip.pkl', 'wb') as f:    #Pickling
+    pickle.dump(img_embeddings, f)
+with open('text_embeddings_clip.pkl', 'wb') as f:    #Pickling
+    pickle.dump(text_embeddings, f)
 # with open('combine_embeddings_clip.pkl', 'wb') as f:    #Pickling
 #     pickle.dump(combine_embeddings, f)
 
